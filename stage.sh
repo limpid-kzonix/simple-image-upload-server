731e94590140fb580f16bffc54a97aa73ce9be34
Hezzer <balyszyn@gmail.com>
2017-05-25 02:48:43 +0300

    edource : preforming project for deploy (Install NodeJs and SBT)

--- stage.sh
+++ stage.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-sbt clean playGenerateSecret  stage  universal:packageZipTarball
+
+echo 'PRODUCTION'
+target/universal/stage/bin/./kunderadatabasecloud -J-Xms1280M -J-Xmx2120m -J-server -Dconfig.file=target/universal/stage/conf/application.conf  -Dhttp.port=9220 > LOG.txt
