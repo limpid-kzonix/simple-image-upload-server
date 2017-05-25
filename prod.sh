5a09537cf526cd2c52432befd8ca1a1458d88aac
Hezzer <balyszyn@gmail.com>
2017-05-26 01:48:58 +0300

    edource : preforming project for deploy (Install NodeJs and SBT)

--- prod.sh
+++ prod.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-echo 'DISCOPERI'
-
+echo 'Prepare project directory'
+sbt clean
+echo 'Stage before production'
+sbt stage
+echo 'Build distributive'
+sbt dist
 echo 'PRODUCTION'
 target/universal/stage/bin/./kunderadatabasecloud -J-Xms1280M -J-Xmx2120m -J-server -Dconfig.file=target/universal/stage/conf/application.conf  -Dhttp.port=9220
