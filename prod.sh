612b7f2adbcfc2a6504b36be170a78c4d50a8bd8
Hezzer <balyszyn@gmail.com>
2017-05-25 19:33:40 +0300

    edource : preforming project for deploy (Install NodeJs and SBT)

--- prod.sh
+++ prod.sh
@@ -3,4 +3,4 @@
 echo 'DISCOPERI'
 
 echo 'PRODUCTION'
-target/universal/stage/bin/./kunderadatabasecloud -J-Xms1280M -J-Xmx2120m -J-server -Dconfig.file=target/universal/stage/conf/application.conf  -Dhttp.port=9220 > LOG.txt
+target/universal/stage/bin/./kunderadatabasecloud -J-Xms1280M -J-Xmx2120m -J-server -Dconfig.file=target/universal/stage/conf/application.conf  -Dhttp.port=9220 > temp_log.txt
