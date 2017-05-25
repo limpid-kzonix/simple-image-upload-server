61db4947f052fe1e2f70a2265ee1e254b8558c04
Hezzer <balyszyn@gmail.com>
2017-05-25 17:55:47 +0300

    edource : preforming project for deploy (Install NodeJs and SBT)

--- prod.sh
+++ prod.sh
@@ -2,8 +2,5 @@
 
 echo 'DISCOPERI'
 
-echo 'PREPARE STAGE'
-sbt clean playGenerateSecret  stage  dist
-
 echo 'PRODUCTION'
 target/universal/stage/bin/./kunderadatabasecloud -J-Xms1280M -J-Xmx2120m -J-server -Dconfig.file=target/universal/stage/conf/application.conf  -Dhttp.port=9220 > LOG.txt
