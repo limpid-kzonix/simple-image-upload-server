390a1084831479af299d0cb15ca043a89664e089
Hezzer <balyszyn@gmail.com>
2017-05-26 12:20:23 +0300

    edource : preforming project for deploy (Install NodeJs and SBT)

--- prod.sh
+++ prod.sh
@@ -7,4 +7,4 @@ sbt stage
 echo 'Build distributive'
 sbt dist
 echo 'PRODUCTION'
-target/universal/stage/bin/./kunderadatabasecloud -J-Xms1280M -J-Xmx2120m -J-server -Dconfig.file=target/universal/stage/conf/application.conf  -Dhttp.port=9220
+sudo target/universal/stage/bin/./kunderadatabasecloud -J-Xms1280M -J-Xmx2120m -J-server -Dconfig.file=target/universal/stage/conf/application.conf -Dpidfile.path=/var/run/play.pid -Dhttp.port=9220
